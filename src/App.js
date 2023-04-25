60e12601899a0c6df0c78f42f929a50fef192eea
JuliValencia <juliana.valencia1@gmail.com>
2023-04-25 14:54:36 +0200

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://julianavalencia.net/" target="_blank" rel="noreferrer">
+          Juliana Valencia{" "}
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/JuliValencia/my-react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
